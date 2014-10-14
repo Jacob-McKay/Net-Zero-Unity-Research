6365a2e1af654df756a22aeb5ec16667cb43c251
Jacob McKay <mckayj@msoe.edu>
2014-10-14 04:25:25 -0500

    "talks" to web service, which replies "woah buddy, wadya doin!?".  Going forward the Unity app will request particular info for a  particular building, which the web service will happly supply.  This is a good start

---
+++ Assets/Scripts/PostURL.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PostURL : MonoBehaviour {
+	
+	void Start () {
+		
+		string url = "http://127.0.0.1/NetZero";
+		
+
+		WWW www = new WWW(url);
+		
+		StartCoroutine(WaitForRequest(www));
+	}
+	
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+			
+			// check for errors
+			if (www.error == null)
+		{
+			Debug.Log("WWW Ok!: " + www.text);
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}    
+}
--- Assets/Scripts/buildingbuttons.cs
+++ Assets/Scripts/buildingbuttons.cs
@@ -1,34 +1,53 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class buildingbuttons : MonoBehaviour {
+public class buildingbuttons : MonoBehaviour
+{
+		public Transform target;
+		private Color startcolor;
+		//public string ObjectName = "Object";
+		bool  isHighlighted = false;
+		bool  wasClickityClicked = false;
 	
-	public Transform target;
-	private Color startcolor;
-	//public string ObjectName = "Object";
-	bool  isHighlighted = false;
-	
-	void  OnMouseEnter (){
-		isHighlighted = true;
-		startcolor = renderer.material.color;
-		renderer.material.color = Color.red;
-	}
-	
-	void  OnMouseExit (){
-		isHighlighted = false;
-		renderer.material.color = startcolor;
-	}
-	
-	void  OnGUI (){
-		Vector3 screenPos= Camera.main.WorldToScreenPoint (transform.position);
-		
-		if (isHighlighted) {
-		//GUI.BeginGroup (new Rect (screenPos.x,Screen.height-Camera.main.WorldToScreenPoint (renderer.bounds.max).y,500,500));
-			if (GUI.Button ( new Rect(screenPos.x,Screen.height-Camera.main.WorldToScreenPoint (renderer.bounds.max).y,100,30), this.gameObject.name)) {
-				Application.LoadLevel ("MSOE_Apartments"); 
-			}
-		//GUI.EndGroup ();
+		void  OnMouseEnter ()
+		{
+				isHighlighted = true;
+				startcolor = renderer.material.color;
+				renderer.material.color = Color.red;
+				//Debug.Log ("Aw Snap, " + this.target + " was hoverty hovered!!");
 		}
-	}
+	
+		void  OnMouseExit ()
+		{
+				isHighlighted = false;
+				renderer.material.color = startcolor;
+		}
+	
+
+		void OnMouseDown ()
+		{
+				wasClickityClicked = true;
+		}
+
+		void OnMouseUp ()
+		{
+				wasClickityClicked = false;
+		}
+	
+		void  OnGUI ()
+		{
+				Vector3 screenPos = Camera.main.WorldToScreenPoint (transform.position);
+		
+				if (isHighlighted) {
+						//GUI.BeginGroup (new Rect (screenPos.x,Screen.height-Camera.main.WorldToScreenPoint (renderer.bounds.max).y,500,500));
+						if (GUI.Button (new Rect (screenPos.x, Screen.height - Camera.main.WorldToScreenPoint (renderer.bounds.max).y, 100, 30), this.gameObject.name)) {
+								Application.LoadLevel ("MSOE_Apartments"); 
+						}
+						//GUI.EndGroup ();
+				}
+				if (wasClickityClicked) {
+						Debug.Log ("Aw Snap, " + this.target + " was clickity clicked!!");
+				}
+		}
 	
 }
