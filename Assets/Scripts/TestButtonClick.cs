9593e70a57881e0243052dc77cca965692ced5ad
Mark Peterson <petersonm@msoe.edu>
2014-10-22 19:53:27 -0500

    working on button scripts
    nothing special...

---
+++ Assets/Scripts/TestButtonClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButtonClick : MonoBehaviour {
+	//set target
+	public Transform target;
+
+	//private Vector3 newPosition;
+
+	void awake ()
+	{
+		//get initial building floors from children objects and put into an array??
+	}
+
+	public void upButton () {
+		Debug.Log ("up button clicked");
+
+		//transform.position = newPosition;
+
+		}
+	public void downButton () {
+		Debug.Log ("down button clicked");
+	}
+
+
+	//public void downButton (string text) {
+	//			Debug.Log (text);
+	//	}
+}
