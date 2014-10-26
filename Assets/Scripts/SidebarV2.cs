4264d537a06e1a09d65922678af78d5814ff29d1
petersonm <petersonm@msoe.edu>
2014-10-26 17:53:25 -0500

    misc
    nothing special

--- Assets/Scripts/SidebarV2.cs
+++ Assets/Scripts/SidebarV2.cs
@@ -2,39 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class SidebarV2 : MonoBehaviour {
+public class SidebarV2 : MonoBehaviour
+{
 
-	float MyBoxLeft= Screen.width;
-	float sidebarclosed= Screen.width;
-	float sidebaropen= Screen.width - 400.0F;
-	int SidebarInt = 0;
+		float MyBoxLeft = Screen.width;
+		float sidebarclosed = Screen.width;
+		float sidebaropen = Screen.width - 400.0F;
+		int SidebarInt = 0;
+		public float smoothTime = 0.3F;
+		private Vector3 velocity = Vector3.zero;
+	public Transform splitfloor;
 
-	public float smoothTime = 0.3F;
-	private Vector3 velocity = Vector3.zero;
-
-	void  OnMouseEnter (){
-			StartCoroutine(AnimateBox());
-	}
-
-	void  Update (){
-	}
-
-	//sidebar animation
-	public IEnumerator  AnimateBox (){
-
-		if (SidebarInt == 0) {			
-			while (MyBoxLeft > sidebaropen) {
-				MyBoxLeft-=20.0F;
-				SidebarInt = 1;
-				yield return 0;
-			}			
-		} 
-		else if (SidebarInt == 1) {	
-			while (MyBoxLeft < sidebarclosed) {
-				MyBoxLeft+=20.0f;
-				SidebarInt = 0;
-				yield return 0;
-			}
+		void Start ()
+		{
+		//Transform splitfloor = this.transform;
+		Vector3 newPOS = new Vector2 (splitfloor.position.x, splitfloor.position.y + 3000);
+		splitfloor.position = newPOS;
+		Debug.Log (splitfloor.position.x);
+		Debug.Log (splitfloor.position.y);
+		Debug.Log (splitfloor.position.y+3000);
 		}
-	}
+
+		void  OnMouseEnter ()
+		{
+			
+		}
+
+		void  Update ()
+		{
+		}
 }
